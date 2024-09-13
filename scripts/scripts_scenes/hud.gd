98dc0b9898344809d5fcbb55acbe2ad5f7044a1f
Max <maxence.vh54@gmail.com>
2024-09-13 18:52:46 +0200

    ajout endurance et potions

--- scripts/scripts_scenes/hud.gd
+++ scripts/scripts_scenes/hud.gd
@@ -3,6 +3,8 @@ extends CanvasGroup
 # variables globales
 @onready var joueur = $/root/Main/Joueur
 @onready var barre_vie_joueur = $Vie_joueur
+@onready var barre_endurance_joueur = $Endurance_joueur
+@onready var compteur_potions_vie = $Compteur_potions_vie
 
 func _ready():
 	# initialisation de la barre de vie
@@ -10,6 +12,23 @@ func _ready():
 	barre_vie_joueur.value = joueur.vie
 	joueur.connect("modification_vie", Callable(self, "_lorsque_modification_vie_joueur"))
 	
+	# initialisation de la barre d'endurance
+	barre_endurance_joueur.max_value = joueur.endurance_max
+	barre_endurance_joueur.value = joueur.endurance
+	joueur.connect("modification_endurance", Callable(self, "_lorsque_modification_endurance_joueur"))
+	
+	# initialisation du compteur de potions
+	compteur_potions_vie.text = str(joueur.nombre_potions_vie)
+	joueur.connect("modification_nombre_potions_vie", Callable(self, "_lorsque_modification_nombre_potions_vie"))
+	
 # actualisation lorsque la vie du joueur est modifiée
 func _lorsque_modification_vie_joueur(nouvelle_valeur_vie_joueur):
 	barre_vie_joueur.value = nouvelle_valeur_vie_joueur
+
+# actualisation losque l'endurance du joueur est modifiée
+func _lorsque_modification_endurance_joueur(nouvelle_valeur_endurance_joueur):
+	barre_endurance_joueur.value = nouvelle_valeur_endurance_joueur
+
+# actualisation lorsque le nombre de potions de vie du joueur est modifié
+func _lorsque_modification_nombre_potions_vie(nouveau_nombre_potions_vie):
+	compteur_potions_vie.text = str(nouveau_nombre_potions_vie)
--- scripts/scripts_scenes/joueur.gd
+++ scripts/scripts_scenes/joueur.gd
@@ -2,16 +2,22 @@ extends Area2D
 
 # Variable globales 
 @onready var noms_collision_layers = $Noms_collision_layers.noms_collision_layers
+@onready var regeneration_en_cours = false
 # Bloquages
 var bloquage_input = false
 var bloquage_rotation = false
 var bloquage_direction = false
 var bloquage_animations = false
+var bloquage_regeneration = false
 # Statistiques du joueur
 @export var vitesse = 400
-@export var vie = 100
 @export var vie_max = 100
+@export var vie = vie_max
 @export var degats = 50
+@export var endurance_max = 50
+@export var endurance = endurance_max
+@export var nombre_potions_vie = 3
+@export var efficacite_potions_vie = 40
 var direction = Vector2.ZERO
 var orientation
 # Actions du joueur
@@ -22,6 +28,7 @@ var input_deplacement_bas = false
 var input_attaque = false
 var input_verrouillage = false
 var input_roulade = false
+var input_potion_vie = false
 # Taille de l'écran
 @onready var screen_size = get_viewport_rect().size
 # Noeuds
@@ -29,8 +36,11 @@ var input_roulade = false
 @onready var animations = $Animations
 @onready var composant_degats = $Composant_degats 
 @onready var attaque = $Attaque
+@onready var timer_regeneration = $Timer_regenaration
 # Signaux
 signal modification_vie(nouvelle_valeur_vie)
+signal modification_endurance(nouvelle_valeur_endurance)
+signal modification_nombre_potions_vie(nouveau_nombre_potions_vie)
 
 func _ready():
 	
@@ -51,6 +61,7 @@ func _process(delta):
 	input_attaque = false
 	input_verrouillage = false
 	input_roulade = false
+	input_potion_vie = false
 	if not bloquage_input:
 		input_deplacement_droite = Input.is_action_pressed("Deplacement_droite")
 		input_deplacement_gauche = Input.is_action_pressed("Deplacement_gauche")
@@ -59,6 +70,7 @@ func _process(delta):
 		input_attaque = Input.is_action_just_pressed("Attaque")
 		input_verrouillage = Input.is_action_just_pressed("Verrouillage")
 		input_roulade = Input.is_action_just_pressed("Roulade")
+		input_potion_vie = Input.is_action_just_pressed("Potion_vie")
 		
 		# Lecture de l'animation d'Idle si le joueur ne se déplace pas
 		if direction.length() == 0:
@@ -115,25 +127,50 @@ func _process(delta):
 			if orientation == "droite":
 				animations.play("Marche_droite")
 
-		# Attaque 
-	if input_attaque:
+	# Attaque 
+	if input_attaque and endurance > 0:
+		# Modification de l'endurance
+		endurance -= 15
+		emit_signal("modification_endurance", endurance)
+		# Lecture de l'animation
 		animations.play("Attaque")
 		bloquage_input = true
+		bloquage_regeneration = true
 
 	# Verrouillage
 	if input_verrouillage:
 		bloquage_rotation = not bloquage_rotation
+	# Si la rotation est bloquée (verrouillage en cours)
+	if bloquage_rotation:
+		verrouillage()
+
+	# Utilisation d'une potion de vie
+	if input_potion_vie and nombre_potions_vie > 0 and vie < vie_max:
+		animations.play("Potion")
+		bloquage_input = true
+		bloquage_animations = true
 
 	# Roulade
-	if input_roulade:
+	if input_roulade and endurance > 0  and direction.length() > 0: 
+		# Modification de l'endurance
+		endurance -= 15
+		emit_signal("modification_endurance", endurance)
+		# Lecture de l'animation
 		animations.play("Roulade")
 		bloquage_input = true
 		bloquage_direction = true
 		bloquage_animations = true
+		bloquage_regeneration = true
 
-	# Si la rotation est bloquée (verrouillage en cours)
-	if bloquage_rotation:
-		verrouillage()
+	# Regeneration
+	if not bloquage_regeneration and endurance < endurance_max:
+		# Temps d'inactivité
+		if timer_regeneration.is_stopped():
+			timer_regeneration.start()
+		# Regeneration
+		if regeneration_en_cours:
+			endurance += 0.5
+			emit_signal("modification_endurance", endurance)
 
 	# Modification de la position du joueur 
 	position += direction.normalized() * delta * vitesse
@@ -170,13 +207,26 @@ func verrouillage():
 
  # Fonction de fin d'animation
 func _on_animations_animation_finished(_anim_name):
-		# Si les inputs sont bloqués
+	# Désactivation des bloquages
 	if bloquage_input:
 		bloquage_input = false
 	if bloquage_direction:
 		bloquage_direction = false
 	if bloquage_animations:
 		bloquage_animations = false
+	if bloquage_regeneration:
+		bloquage_regeneration = false
+	# Fin d'animation de potion de vie
+	if _anim_name == "Potion":
+		# Modification de la vie actuelle
+		if vie_max - vie < efficacite_potions_vie:
+			vie = vie_max
+		else:
+			vie += efficacite_potions_vie
+		emit_signal("modification_vie", vie)
+		# Modification du nombre de potions
+		nombre_potions_vie -= 1
+		emit_signal("modification_nombre_potions_vie", nombre_potions_vie)
 
 # En cas de collision
 func _on_area_entered(area):
@@ -191,3 +241,10 @@ func _on_composant_degats_degats_pris(vie_actuelle):
 	vie = vie_actuelle
 	# Emmision d'un signal (nottament pour le HUD)
 	emit_signal("modification_vie", vie)
+
+# En cas de regeneration
+func _on_timer_regenaration_timeout():
+	if not bloquage_regeneration and endurance < endurance_max:
+		regeneration_en_cours = true
+	else:
+		regeneration_en_cours = false
